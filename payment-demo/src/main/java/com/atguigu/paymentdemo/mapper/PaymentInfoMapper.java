b1530dd47f66d0ac0115443834a29ded01e82297
xiaobei222 <129251917+xiaobei222@users.noreply.github.com>
2023-03-30 16:36:51 +0800

    Delete payment-demo directory

--- payment-demo/src/main/java/com/atguigu/paymentdemo/mapper/PaymentInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.paymentdemo.mapper;
-
-import com.atguigu.paymentdemo.entity.PaymentInfo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface PaymentInfoMapper extends BaseMapper<PaymentInfo> {
-}
